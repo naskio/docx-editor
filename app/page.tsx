44aca4327266c211ad0fd4483400b2eb839e3463
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2025-02-09 10:06:48 +0100

    chore(lint): setup Prettier
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Page() {
-    return <>
-        <Image src="/logo.svg" alt="Logo" width={256} height={64}/>
-        <h1>Hello, Next.js!</h1>
+  return (
+    <>
+      <Image src='/logo.svg' alt='Logo' width={256} height={64} />
+      <h1>Hello, Next.js!</h1>
     </>
+  );
 }
