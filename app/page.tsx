5b2dfc28b4e722db9c95fc5d132456b42149e57f
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2025-02-09 12:31:25 +0100

    fix: use base path with image
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 
 export default function Page() {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
   return (
     <>
-      <Image src='/logo.svg' alt='Logo' width={256} height={64} />
+      <Image src={`${basePath}/logo.svg`} alt='Logo' width={256} height={64} />
       <h1 className='text-3xl font-bold underline'>
         Hello, Next.js with Tailwind CSS!
       </h1>
