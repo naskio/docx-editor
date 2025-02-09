cb3b8fcda3ae02abf40455522358b369894fb9e5
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2025-02-09 11:45:20 +0100

    test: setup Jest with JSDom
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

---
+++ __tests__/jest.setup.ts
@@ -0,0 +1,1 @@
+import '@testing-library/jest-dom'; // extends jest expect with jest-dom matchers
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Page() {
   return (
     <>
       <Image src='/logo.svg' alt='Logo' width={256} height={64} />
-      <h1>Hello, Next.js!</h1>
-      <h1 className='text-3xl font-bold underline'>Hello, Tailwind CSS!</h1>
+      <h1 className='text-3xl font-bold underline'>
+        Hello, Next.js with Tailwind CSS!
+      </h1>
     </>
   );
 }
