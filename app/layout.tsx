b9cf8684005b32635424ea9522bfde649d3c52c1
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2025-02-09 15:28:34 +0100

    feat: setup fonts
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css'; // These styles apply to every route in the application
+
+// Define the font classes
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
+  const fontClassNames = `${geistSans.variable} ${geistMono.variable} antialiased`;
+
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className={fontClassNames}>{children}</body>
     </html>
   );
 }
