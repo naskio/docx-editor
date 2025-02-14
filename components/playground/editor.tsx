feb414cf9c87d6b2884368cb98c0a50f3db5ac3a
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2025-02-14 23:14:56 +0100

    feat: add editor tabs management
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

--- components/playground/editor.tsx
+++ components/playground/editor.tsx
@@ -1,26 +1,86 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { buildDocument, documentToBlob } from '@/lib/docx';
+import { XIcon, Loader2Icon } from 'lucide-react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { usePreviewStore } from '@/store/preview-store-provider';
 import { useDocumentsStore } from '@/store/documents-store-provider';
+import { buildDocument, documentToBlob } from '@/lib/docx';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Separator } from '@/components/ui/separator';
 
 export function Editor() {
   const { setPreview } = usePreviewStore((state) => state);
-  const { documents } = useDocumentsStore((state) => state);
+  const { openTabs, activeTab, setActiveTab, documents, closeDocument } =
+    useDocumentsStore((state) => state);
   const [isCompiling, setIsCompiling] = useState<boolean>(false);
 
   useEffect(() => {
     if (documents.length) {
-      const selected = documents[0];
-      setIsCompiling(true);
-      const doc = buildDocument(selected.text);
-      documentToBlob(doc).then((blob) => {
-        setPreview(selected.name, blob);
-        setIsCompiling(false);
-      });
+      const selected = documents.find((doc) => doc.name === activeTab);
+      if (selected) {
+        setIsCompiling(true);
+        const doc = buildDocument(selected.text);
+        documentToBlob(doc).then((blob) => {
+          setPreview(selected.name, blob);
+          setIsCompiling(false);
+        });
+      }
     }
-  }, [documents, setPreview]);
+  }, [documents, setPreview, activeTab]);
 
-  return <div>{isCompiling ? 'Compiling...' : 'Compiled'}</div>;
+  return (
+    <>
+      {Boolean(openTabs?.length) && (
+        <Tabs value={activeTab} onValueChange={setActiveTab} className='h-full'>
+          <TabsList className='bg-sidebar text-sidebar-foreground h-13 w-full justify-start gap-x-1 rounded-none'>
+            {openTabs.map((docName) => (
+              <TabsTrigger
+                value={docName}
+                key={docName}
+                autoFocus={docName === activeTab}
+                className='h-9 gap-x-2 border px-2 focus-visible:h-9 focus-visible:ring-1 focus-visible:ring-offset-0'
+                onKeyDown={(e) => {
+                  // if delete or backspace is pressed => close the document
+                  if (e.key === 'Delete' || e.key === 'Backspace') {
+                    closeDocument(docName);
+                    e.preventDefault();
+                  }
+                }}
+              >
+                {docName}
+                {isCompiling && activeTab === docName ? (
+                  <Loader2Icon className='size-6 animate-spin motion-reduce:animate-[spin_1.5s_linear_infinite]' />
+                ) : (
+                  <XIcon
+                    className={cn(
+                      buttonVariants({ variant: 'ghost', size: 'icon' }),
+                      `text-sidebar-accent-foreground size-6`
+                    )}
+                    onPointerDown={(e) => {
+                      closeDocument(docName);
+                      e.preventDefault();
+                    }}
+                  />
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <Separator />
+          {openTabs
+            .map((docName) => {
+              const doc = documents.find((d) => d.name === docName);
+              if (!doc) return;
+              return (
+                <TabsContent value={doc.name} key={doc.name} className='mt-0'>
+                  <div>{doc.text}</div>
+                </TabsContent>
+              );
+            })
+            .filter(Boolean)}
+        </Tabs>
+      )}
+    </>
+  );
 }
--- store/documents-store.ts
+++ store/documents-store.ts
@@ -2,8 +2,12 @@ import { createStore } from 'zustand/vanilla';
 import { persist, devtools } from 'zustand/middleware';
 import type { TextFile } from '@/lib/types';
 
+const MAX_OPEN_TABS = 3;
+
 export type DocumentsState = {
   documents: TextFile[];
+  openTabs: string[];
+  activeTab: string;
 };
 
 export type DocumentsActions = {
@@ -13,6 +17,7 @@ export type DocumentsActions = {
   renameDocument: (oldName: string, newName: string) => void;
   openDocument: (name: string) => void;
   closeDocument: (name: string) => void;
+  setActiveTab: (name: string) => void;
 };
 
 export type DocumentsStore = DocumentsState & DocumentsActions;
@@ -20,6 +25,8 @@ export type DocumentsStore = DocumentsState & DocumentsActions;
 export const initDocumentsStore = (): DocumentsState => {
   return {
     documents: [],
+    openTabs: [],
+    activeTab: ``,
   };
 };
 
@@ -78,11 +85,46 @@ export const createDocumentsStore = (
               if (doc) {
                 doc.atime = new Date();
               }
-              return { documents: [...state.documents] };
+              if (!state.openTabs.includes(name)) {
+                if (state.openTabs.length >= MAX_OPEN_TABS) {
+                  state.openTabs.shift(); // remove the first tab
+                }
+                state.openTabs.push(name);
+              }
+              state.activeTab = name;
+              return {
+                documents: [...state.documents],
+                editorTabs: [...state.openTabs],
+                activeTab: state.activeTab,
+              };
             }),
-          closeDocument: (name) => {
-            console.log(`closeDocument: ${name}`);
-          },
+          closeDocument: (name) =>
+            set((state) => {
+              const index = state.openTabs.indexOf(name);
+              if (index !== -1) {
+                state.openTabs = state.openTabs.filter((tab) => tab !== name);
+                // check if the active tab is the one being closed
+                if (state.activeTab === name) {
+                  if (state.openTabs.length) {
+                    // if there are still tabs open
+                    if (index === 0) {
+                      // if first tab, set the active tab to the next one
+                      state.activeTab = state.openTabs[index];
+                    } else {
+                      // if not the first tab, set the active tab to the previous one
+                      state.activeTab = state.openTabs[index - 1];
+                    }
+                  } else {
+                    state.activeTab = ``;
+                  }
+                }
+              }
+              return {
+                editorTabs: [...state.openTabs],
+                activeTab: state.activeTab,
+              };
+            }),
+          setActiveTab: (name) => set({ activeTab: name }),
         }),
         {
           name: 'documents-storage', // name of the item in the storage (must be unique)
